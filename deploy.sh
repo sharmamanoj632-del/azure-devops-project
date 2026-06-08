06b0412666855e7890d4b93f43327891d8d5cdc6
Ubuntu <azureuser@devops-vm.ecgrw2dajo4uhas04hosqkggye.rx.internal.cloudapp.net>
2026-06-08 19:18:55 +0000

    fix deployment script

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,8 @@
 cd ~/myapp
 git pull origin main
 docker build -t manoj-app .
-docker stop $(docker ps -q) || true
-docker run -d -p 80:80 manoj-app
+
+docker stop manoj-container || true
+docker rm manoj-container || true
+
+docker run -d -p 80:80 --name manoj-container manoj-app
